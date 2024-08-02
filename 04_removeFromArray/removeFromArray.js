88f6d8b1ca7d7431233b2749a1794ea6e3523350
edjozero <ej21serafin@gmail.com>
2024-08-01 22:09:02 -0700

    Add code that satisfied first test of test list

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, arg) {
+    let newArray = [];
+    for (let i = 0; i < arr.length; i++) {
+        if(arr[i] !== arg){
+            newArray.push(arr[i]);
+        }       
+    }
+
+    return newArray;
 };
 
 // Do not edit below this line
