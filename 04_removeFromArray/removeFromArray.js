c92a2b425db80d20454e396c9a80444280170eb2
edjozero <ej21serafin@gmail.com>
2024-08-05 13:06:46 -0700

    Add code to satisfy two test scenarios Checked to see how a for loop and forEach could be used here

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,12 +1,29 @@
-const removeFromArray = function(arr, arg) {
-    let newArray = [];
-    for (let i = 0; i < arr.length; i++) {
-        if(arr[i] !== arg){
-            newArray.push(arr[i]);
-        }       
-    }
+const removeFromArray = function(arr, ...args) {
+    // let newArray = [];
+    // for (let i = 0; i < arr.length; i++) {
+    //     if(arr[i] !== arg){
+    //         newArray.push(arr[i]);
+    //     }       
+    // }
 
-    return newArray;
+    // return newArray;
+
+    let newArr = [];
+
+    arr.forEach(item => {
+        if(!args.includes(item)){
+            newArr.push(item);
+        }
+    });
+
+    // for(let i=0; i<arr.length; i++){
+    //     if(!args.includes(arr[i])){
+    //         newArr.push(arr[i]);
+    //     }
+    // }
+
+    console.log(newArr);
+    return newArr;
 };
 
 // Do not edit below this line
