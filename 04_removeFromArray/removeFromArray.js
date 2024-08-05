7552e2840dd1c1aa6ea68c91b475ec1f7ffd4797
edjozero <ej21serafin@gmail.com>
2024-08-05 13:54:11 -0700

    Confirm that all tests pass

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,28 +1,13 @@
 const removeFromArray = function(arr, ...args) {
-    // let newArray = [];
-    // for (let i = 0; i < arr.length; i++) {
-    //     if(arr[i] !== arg){
-    //         newArray.push(arr[i]);
-    //     }       
-    // }
-
-    // return newArray;
 
     let newArr = [];
 
-    arr.forEach(item => {
-        if(!args.includes(item)){
-            newArr.push(item);
+    for(let i=0; i<arr.length; i++){
+        if(!args.includes(arr[i])){
+            newArr.push(arr[i]);
         }
-    });
+    }
 
-    // for(let i=0; i<arr.length; i++){
-    //     if(!args.includes(arr[i])){
-    //         newArr.push(arr[i]);
-    //     }
-    // }
-
-    console.log(newArr);
     return newArr;
 };
 
