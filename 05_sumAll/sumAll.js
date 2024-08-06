46ff1e46d935f49155e966967b38d67d3b3cc1e0
edjozero <ej21serafin@gmail.com>
2024-08-05 21:21:33 -0700

    Add function body to solve problem Satisfied test 1

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    // the following worked to satisfy test 1
+    let sum = 0;
 
+    // the condition in if statement is for some of remaining tests
+    for(let i=min; i<=max; i++){
+        if(i < 0 || typeof i === "string"){
+            return "ERROR";
+        }else{
+            sum += i;
+        }
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
