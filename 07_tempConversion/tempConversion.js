835e1bee9c1f0f6a1ba0d1ecf454a5f3c57c76b1
edjozero <ej21serafin@gmail.com>
2024-08-07 09:06:32 -0700

    Add formula of conversion for both temperature types formula for both functions added added round to result of each function conversion to round to one decimal space

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fah) {
+  // formula for F to C: C = 5/9 * (F - 32)
+  let celsius = 5/9 * (fah - 32);
+  return Math.round(celsius);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+  // formula for C to F: (C * 9/5) + 32
+  let fahrenheit = (cel * 9/5) + 32;
+  return Math.round(fahrenheit);
 };
 
 // Do not edit below this line
