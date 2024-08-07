aafb3617c2c282937be329002f4b94e08654c1a3
edjozero <ej21serafin@gmail.com>
2024-08-07 11:31:04 -0700

    Add condition to functions for use of temps with decimals

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,21 @@
 const convertToCelsius = function(fah) {
   // formula for F to C: C = 5/9 * (F - 32)
   let celsius = 5/9 * (fah - 32);
-  return Math.round(celsius);
+  if(!Number.isInteger(celsius)){
+    return Math.round(celsius * 10) / 10;
+  }else{
+    return Math.round(celsius);
+  }
 };
 
 const convertToFahrenheit = function(cel) {
   // formula for C to F: (C * 9/5) + 32
   let fahrenheit = (cel * 9/5) + 32;
-  return Math.round(fahrenheit);
+  if(!Number.isInteger(fahrenheit)){
+    return Math.round(fahrenheit * 10) / 10;
+  }else{
+    return Math.round(fahrenheit);
+  }
 };
 
 // Do not edit below this line
