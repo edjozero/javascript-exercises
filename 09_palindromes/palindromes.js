6e12a8e0b7c3efcf4c9c87f61284a6d782358867
edjozero <ej21serafin@gmail.com>
2024-08-11 21:58:13 -0700

    Add code to replace unwanted characters from original string Doing this enables one to be able to compare the original string to the reversed version This satisfied remaining tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,12 +1,22 @@
 const palindromes = function (string) {
     // compare a string to its reversed self
-    let strArr = string.split("").reverse().join("");
+    // let strArr = string.split("").reverse().join("");
 
-    if(string === strArr){
-        return true;
-    }else{
-        return false;
-    }
+    // if(string === strArr){
+    //     return true;
+    // }else{
+    //     return false;
+    // }
+
+    // use regex to remove characters we dont want in original string to make it easy to compare to its reversed version
+    let excludedItems = /[^a-zA-Z0-9]/g;
+
+    const cleanStr = string.replace(excludedItems, "").toLowerCase();
+    
+    const reverseStr = cleanStr.split("").reverse().join("");
+
+    return cleanStr === reverseStr;
+    
 };
 
 // Do not edit below this line
