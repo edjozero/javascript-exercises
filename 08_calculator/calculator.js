3b600a7effd200a6cd0c66b25b3ec01846431dc7
edjozero <ej21serafin@gmail.com>
2024-09-25 16:14:20 -0700

    Add code using reduce to solve sum and multiply function

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -7,15 +7,21 @@ const subtract = function(a, b) {
 };
 
 const sum = function(args) {
-	let sum = 0;
-  args.forEach(el => sum += el);
-  return sum;
+	// let sum = 0;
+  // args.forEach(el => sum += el);
+  // return sum;
+
+  // using reduce
+  return args.reduce((total, item) => total + item, 0);
 };
 
 const multiply = function(args) {
-  let sum = 1;
-  args.forEach(el => sum *= el);
-  return sum;
+  // let sum = 1;
+  // args.forEach(el => sum *= el);
+  // return sum;
+
+  // use reduce
+  return args.reduce((sum, item) => sum * item);
 };
 
 const power = function(a,b) {
