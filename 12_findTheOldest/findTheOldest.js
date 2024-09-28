7c187f40617c5e4d0bcd696cf56a72cb20d577b6
edjozero <ej21serafin@gmail.com>
2024-09-27 17:02:35 -0700

    Add sorting to objects and return the oldest person

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function(peeps) {
+    const sortedPeeps = peeps
+        .sort((a,b) => {
+            const firstPeep = a.yearOfDeath - a.yearOfBirth;
+            const nextPeep = b.yearOfDeath - b.yearOfBirth;
 
+            return firstPeep > nextPeep ? -1 : 1;
+        }
+    );
+
+    return sortedPeeps[0];
 };
 
 // Do not edit below this line
