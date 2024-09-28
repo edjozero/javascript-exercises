5f6d674847a6065a431fb0601309433cdf60b765
edjozero <ej21serafin@gmail.com>
2024-09-27 22:31:58 -0700

    Add if statement to cover if an object item had no date of death

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,16 @@
 const findTheOldest = function(peeps) {
     const sortedPeeps = peeps
         .sort((a,b) => {
+
+            if(a.yearOfDeath === undefined){
+                a.yearOfDeath = new Date().getFullYear();
+            }
+
+            if(b.yearOfDeath === undefined){
+                b.yearOfDeath = new Date().getFullYear();
+            }
+
+
             const firstPeep = a.yearOfDeath - a.yearOfBirth;
             const nextPeep = b.yearOfDeath - b.yearOfBirth;
 
